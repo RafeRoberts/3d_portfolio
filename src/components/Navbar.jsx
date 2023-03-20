9efeae7e3b0514b13082709bb50b429004d60b2a
raferoberts <rafe@enable360.com.au>
2023-03-20 15:43:55 +0800

    hero image working

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -11,7 +11,7 @@ const Navbar = () => {
   const [toggle, setToggle] = useState (false);
   return (
     <nav
-      className={`${styles.paddindX} w-full flex items-center py-5 fixed top-0 z-20 bg-primary`}
+      className={`${styles.paddingX} w-full flex items-center py-5 fixed top-0 z-20 bg-primary`}
       >
         <div className='w-full flex justify-between items-center max-w-7xl mx-auto'>
           <Link 
@@ -47,6 +47,24 @@ const Navbar = () => {
                 className='w-[28px] h-[28px] object-contain cursor-pointer'
                 onClick = {() => setToggle(!toggle)} />
                 <div className={`${!toggle ? 'hidden' : 'flex' } p-6 black-gradient absolute top-20 right-0 mx-4 my-2 min-w-[140px] z-10 rounded-xl`}>
+                <ul className='list-none flex justify-end items-start flex-col gap-4'>
+                {navLinks.map((Link) => (
+                  <li
+                    key={Link.id}
+                    className={`${
+                      active === Link.title  
+                        ? "text-white"
+                        : "text-secondary"
+                    } hover:text-white text-[18px] font-medium cursor-pointer`}
+                    onClick={() => {
+                      setToggle(!toggle);
+                      setActive(Link.title);
+                    }}
+                  >
+                    <a href={`#${Link.id}`}>{Link.title}</a>
+                  </li>
+                ))}
+              </ul>
 
                 </div>
 
